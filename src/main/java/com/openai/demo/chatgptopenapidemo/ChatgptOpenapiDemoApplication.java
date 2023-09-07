af55e574d782ecf2fd24d3616dd4c5c284efd018
Abinandan02 <Abinandannaik@gmail.com>
2023-09-07 23:04:38 +0800

    first commit

---
+++ src/main/java/com/openai/demo/chatgptopenapidemo/ChatgptOpenapiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.openai.demo.chatgptopenapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptOpenapiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptOpenapiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openai/demo/chatgptopenapidemo/ChatgptOpenapiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openai.demo.chatgptopenapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptOpenapiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
